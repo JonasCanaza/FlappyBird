38a6677191315c7e33598e6d2ebaa69f9b95cafd
Jonas <jonascanaza@gmail.com>
2025-11-21 09:36:11 -0300

    refactor the background code to avoid magic numbers

--- src/background/BackgroundManager.cpp
+++ src/background/BackgroundManager.cpp
@@ -16,32 +16,45 @@ namespace backgroundManager
 	static drw::SpriteData foreground;
 	static float foregroundSpeed;
 
+	static const vec::Vector2 DEFAULT_SPRITE_SIZE = { 1.0f, 1.0f };
+	static const vec::Vector2 DEFAULT_SPRITE_OFFSET = { 0.0f, 0.0f };
+
+	static const float BACK_SPEED = 0.05f;
+	static const float MID_SPEED = 0.10f;
+	static const float FRONT_SPEED = 0.20f;
+	static const float FOREGROUND_SPEED = 0.35f;
+
+	static const vec::Vector2 DRAW_POSITION = { 0.5f, 0.5f };
+
+	static const float WRAP_LIMIT = -1.0f;
+	static const float WRAP_OFFSET = 1.0f;
+
 	static void DrawLayer(drw::SpriteData& layer);
 
 	void Init()
 	{
 		back.file = "res/sprites/backgrounds/back.png";
-		back.size = { 1.0f, 1.0f };
-		back.offset = { 0.0f, 0.0f };
-		backSpeed = 0.05f;
+		back.size = DEFAULT_SPRITE_SIZE;
+		back.offset = DEFAULT_SPRITE_OFFSET;
+		backSpeed = BACK_SPEED;
 		back.id = drw::InitSpriteData(back);
 
 		mid.file = "res/sprites/backgrounds/mid.png";
-		mid.size = { 1.0f, 1.0f };
-		mid.offset = { 0.0f, 0.0f };
-		midSpeed = 0.10f;
+		mid.size = DEFAULT_SPRITE_SIZE;
+		mid.offset = DEFAULT_SPRITE_OFFSET;
+		midSpeed = MID_SPEED;
 		mid.id = drw::InitSpriteData(mid);
 
 		front.file = "res/sprites/backgrounds/front.png";
-		front.size = { 1.0f, 1.0f };
-		front.offset = { 0.0f, 0.0f };
-		frontSpeed = 0.20f;
+		front.size = DEFAULT_SPRITE_SIZE;
+		front.offset = DEFAULT_SPRITE_OFFSET;
+		frontSpeed = FRONT_SPEED;
 		front.id = drw::InitSpriteData(front);
 
 		foreground.file = "res/sprites/backgrounds/foreground.png";
-		foreground.size = { 1.0f, 1.0f };
-		foreground.offset = { 0.0f, 0.0f };
-		foregroundSpeed = 0.35f;
+		foreground.size = DEFAULT_SPRITE_SIZE;
+		foreground.offset = DEFAULT_SPRITE_OFFSET;
+		foregroundSpeed = FOREGROUND_SPEED;
 		foreground.id = drw::InitSpriteData(foreground);
 	}
 
@@ -52,24 +65,24 @@ namespace backgroundManager
 		front.offset.x -= frontSpeed * rend::deltaTime;
 		foreground.offset.x -= foregroundSpeed * rend::deltaTime;
 
-		if (back.offset.x <= -1.0f)
+		if (back.offset.x <= WRAP_LIMIT)
 		{
-			back.offset.x += 1.0f;
+			back.offset.x += WRAP_OFFSET;
 		}
 
-		if (mid.offset.x <= -1.0f)
+		if (mid.offset.x <= WRAP_LIMIT)
 		{
-			mid.offset.x += 1.0f;
+			mid.offset.x += WRAP_OFFSET;
 		}
 
-		if (front.offset.x <= -1.0f)
+		if (front.offset.x <= WRAP_LIMIT)
 		{
-			front.offset.x += 1.0f;
+			front.offset.x += WRAP_OFFSET;
 		}
 
-		if (foreground.offset.x <= -1.0f)
+		if (foreground.offset.x <= WRAP_LIMIT)
 		{
-			foreground.offset.x += 1.0f;
+			foreground.offset.x += WRAP_OFFSET;
 		}
 	}
 
@@ -91,7 +104,7 @@ namespace backgroundManager
 
 	static void DrawLayer(drw::SpriteData& layer)
 	{
-		drw::Sprite(drw::spriteDataList[layer.id], { 0.5f, 0.5f }, layer.size, layer.offset);
-		drw::Sprite(drw::spriteDataList[layer.id], { 0.5f, 0.5f }, layer.size, { layer.offset.x + 1.0f, layer.offset.y });
+		drw::Sprite(drw::spriteDataList[layer.id], DRAW_POSITION, layer.size, layer.offset);
+		drw::Sprite(drw::spriteDataList[layer.id], DRAW_POSITION, layer.size, { layer.offset.x + WRAP_OFFSET, layer.offset.y });
 	}
 }
